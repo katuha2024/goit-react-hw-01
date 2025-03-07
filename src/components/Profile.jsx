59f1dae17e6a4e19cc6885c8704d557550c372ec
katuha2024 <ukrbrend2020@gmail.com>
2025-03-07 08:20:53 +0200

    1

---
+++ src/components/Profile.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Profile.css';
+
+function Profile({ name, tag, location, image, stats }) {
+  return (
+    <div className="profile">
+      <div className="profile-header">
+        <img
+          src={image}
+          alt="User avatar"
+          className="profile-avatar"
+        />
+        <p className="profile-name">{name}</p>
+        <p className="profile-tag">@{tag}</p>
+        <p className="profile-location">{location}</p>
+      </div>
+
+      <ul className="profile-stats">
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Profile;
